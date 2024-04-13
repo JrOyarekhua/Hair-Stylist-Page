b32f883f83603aa92951cfd3b8ab22c5579b42a9
JrOyarekhua <Goyarekhua@oglethorpe.edu>
2024-04-13 02:04:27 -0400

    working on slider

---
+++ src/images/Portfolio images/Hair Webpage/image.js
@@ -0,0 +1,24 @@
+import image1 from "./IMG_0248.JPG";
+import image2 from "./IMG_0721.JPG";
+import image3 from "./IMG_0775.JPG";
+import image4 from "./IMG_1234.JPG";
+import image5 from "./IMG_1286.JPG";
+import image6 from "./IMG_2286.JPG";
+import image7 from "./IMG_2383.JPG";
+import image8 from "./IMG_4119.JPG";
+import image9 from "./IMG_5550.JPG";
+import image10 from "./IMG_6469.JPG";
+import image11 from "./IMG_6767.JPG";
+import image12 from "./IMG_6991.JPG";
+import image13 from "./IMG_7830.JPG";
+import image14 from "./IMG_8655.JPG";
+import image15 from "./IMG_9096.JPG";
+import image16 from "./IMG_9299.JPG";
+import image17 from "./IMG_9791.JPG";
+
+const images = [];
+for (let i = 1; i <= 17; i++) {
+  images.push(eval(`image${i}`));
+}
+
+export default images;
--- src/pages/PortfolioPage/components/main/Portfolio/Portfolio.jsx
+++ src/pages/PortfolioPage/components/main/Portfolio/Portfolio.jsx
@@ -1,8 +1,53 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleArrowLeft,
+  faCircleArrowRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import images from "../../../../../images/Portfolio images/Hair Webpage/image";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "./portfolio.css";
+
 function Portfolio() {
   return (
-    <section>
-      <h2>Portfolio</h2>
-      <p>Browse through my stunning selections</p>
+    <section className="portfolio container">
+      <div className="portfolio-text">
+        <h2>Portfolio</h2>
+        <p>browse through my stunning selections</p>
+      </div>
+      <Swiper
+        modules={[Navigation, Pagination]}
+        spaceBetween={15}
+        slidesPerView={3}
+        slidesPerGroup={4}
+        navigation={{
+          prevEl: ".swiper-button-prev",
+          nextEl: ".swiper-button-next",
+        }}
+        pagination={{ el: ".swiper-pagination", clickable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
+        {images.map((image, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <img src={image} alt={`image${index}`} />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+      <div className="slider-controller">
+        <div className="swiper-button-prev">
+          <FontAwesomeIcon icon={faCircleArrowLeft} />
+        </div>
+        <div className="swiper-button-next">
+          <FontAwesomeIcon icon={faCircleArrowRight} />
+        </div>
+        <div className="swiper-pagination"></div>
+      </div>
     </section>
   );
 }
